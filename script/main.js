35d5fbcd97ef0c753bc86c9a3fef6f99fd28e16b
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-09-30 18:48:00 +0300

    day1

---
+++ script/main.js
@@ -0,0 +1,42 @@
+
+
+//day1
+
+const btnBurger = document.querySelector('.btn-burger');
+const catalog = document.querySelector('.catalog');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.btn-close');
+const subCatalog = document.querySelector('.subcatalog');
+const subcatalogHeader = document.querySelector('.subcatalog-header');
+
+const openMenu = () => {
+    catalog.classList.add('open');
+    overlay.classList.add('active');
+};
+
+const closeMenu = () => {
+    catalog.classList.remove('open');
+    overlay.classList.remove('active');
+};
+
+const openSubMenu = (event) => {
+    event.preventDefault();
+    const itemList = event.target.closest('.catalog-list__item');
+    if (itemList) {
+        subcatalogHeader.innerHTML = itemList.innerHTML;
+        subCatalog.classList.add('subopen');
+    };
+};
+
+btnBurger.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+catalog.addEventListener('click', openSubMenu);
+
+//Убираем меню при нажатии Escape
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        closeMenu();
+    }
+});
